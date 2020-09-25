18f4ef955ee5bdc2aea7c332179f78c76989ebef
MR-aalole <52821144+Temitope25@users.noreply.github.com>
2020-09-25 22:20:16 +0100

    completed all the todo list functionalties

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Todo-list
+
+Now,
+
+- you can add to the list,
+- store the list to the localstorage
+- Filter you list to get the todo of your choice
+- remove a single list on clicking the deleter (x) icon;
+- You can even clear all tasks after they've been done.
--- js/script.js
+++ js/script.js
@@ -2,29 +2,50 @@
 const form = document.querySelector("#todo-form");
 const taskInput = document.querySelector("#task");
 const clearBtn = document.querySelector("#clear-btn");
-// const submitTaskBtn = document.querySelector('.btn');
 const filterInput = document.querySelector("#filter");
 const collection = document.querySelector(".collection");
-// const body = document.querySelector('body')
 const link = document.querySelector("collection-item");
 
 // Add addEventListeners to trigger functions
 const loadEventListeners = () => {
+  document.addEventListener("DOMContentLoaded", getTasks);
   form.addEventListener("submit", addTask);
   collection.addEventListener("click", deleteTask);
   clearBtn.addEventListener("click", clearTasks);
   filterInput.addEventListener("keyup", filterTask);
 };
 
-// Add todos to list to the UI 
+// get tasks from LS
+const getTasks = () => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function (task) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(task));
+    //   2) create anchor element and corresponding delete icon add appropriate classes
+    const link = document.createElement("a");
+    link.innerHTML = `<i class="fa fa-remove"></i>`;
+    link.className = "delete-item secondary-content";
+    //   3) Append the newly created elements to the UL element
+    li.appendChild(link);
+    collection.appendChild(li);
+  });
+};
+
+// Add todos to list to the UI
 const addTask = (e) => {
   if (taskInput.value === "") {
     alert("Enter new task");
   }
-//   1) create list element and add appropriate classes
+  //   1) create list element and add appropriate classes
   const li = document.createElement("li");
   li.className = "collection-item";
-  li.innerText = taskInput.value;
+  li.appendChild(document.createTextNode(taskInput.value));
   //   2) create anchor element and corresponding delete icon add appropriate classes
   const link = document.createElement("a");
   link.innerHTML = `<i class="fa fa-remove"></i>`;
@@ -32,25 +53,74 @@ const addTask = (e) => {
   //   3) Append the newly created elements to the UL element
   li.appendChild(link);
   collection.appendChild(li);
-//   4) clear the input value once the action fires
+  //   4) store the input value into localStorage
+  storeTasksInLocalStorage(taskInput.value);
+  //   5) clear the input value once the action fires
   taskInput.value = "";
   e.preventDefault();
 };
 
+// Get tasks from localStorage
+const storeTasksInLocalStorage = (task) => {
+  // step1) check if there is no task in localStorage
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// DOM ELEMENT CREATOR FUNCTIONS
+// const domElemCreator = () => {
+//   const li = document.createElement("li");
+//   li.className = "collection-item";
+//   li.appendChild(document.createTextNode(task));
+//   //   2) create anchor element and corresponding delete icon add appropriate classes
+//   const link = document.createElement("a");
+//   link.innerHTML = `<i class="fa fa-remove"></i>`;
+//   link.className = "delete-item secondary-content";
+//   //   3) Append the newly created elements to the UL element
+//   li.appendChild(link);
+//   collection.appendChild(li);
+// };
+
+// remove tasks from localStorage
+const removeTaskFromLocStorage = (taskItem) => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach((task, index) => {
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 // Deleter list items
 const deleteTask = (e) => {
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("are you sure?")) {
       e.target.parentElement.parentElement.remove();
+      removeTaskFromLocStorage(e.target.parentElement.parentElement);
     }
   }
 };
-//  Clear all tasks
+//  Clear  task while tasks 
+// Clear all tasks while tasks 
 const clearTasks = () => {
   while (collection.firstChild) {
     collection.firstChild.remove();
   }
+  clearAllFromLstorage();
 };
+//Clear all  task from LS
+const clearAllFromLstorage = () => localStorage.clear();
 //  Filter tasks
 const filterTask = (e) => {
   const text = e.target.value.toLowerCase();
@@ -64,4 +134,5 @@ const filterTask = (e) => {
     }
   });
 };
+
 loadEventListeners();
